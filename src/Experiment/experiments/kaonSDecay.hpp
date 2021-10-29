5b19066cf7428e73c84701721c20420b34621934
Matteo Bonacini <matteo.bonna@gmail.com>
2021-10-29 17:06:48 +0200

    Remove test code in kaonSDecay

--- src/Experiment/experiments/kaonSDecay.hpp
+++ src/Experiment/experiments/kaonSDecay.hpp
@@ -10,14 +10,7 @@ namespace sim {
 
 class KaonSDecay : public Experiment {
  public:
-  inline KaonSDecay() {
-    static Entity* entities[2];
-    entities[0] = new KaonM;
-    entities[1] = new KaonP;
-
-    entities[0]->printAttributes();
-    entities[1]->printAttributes();
-  }
+  inline KaonSDecay() {}
 
   inline void run(int eventCount, int particlePerEvent) override {}
   inline void save(std::string fileName) override {}
